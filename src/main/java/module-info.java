5f81e42eb7f6feba0ada7ec9ecc06df41900f3b6
Simone Amighini <dev@simoneamighini.it>
2024-08-22 14:50:43 +0200

    Add client and server start from command line with arguments
    Add jCommander to pom.
    Implement jCommander arguments parsing in ClientMain and ServerMain.
    Edit Server.getInstance(int port) method to avoid NullPointerException when trying to access logger.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module it.simoneamighini.scala40 {
 
     requires org.controlsfx.controls;
     requires java.logging;
+    requires jcommander;
 
     opens it.simoneamighini.scala40 to javafx.fxml;
     exports it.simoneamighini.scala40;
