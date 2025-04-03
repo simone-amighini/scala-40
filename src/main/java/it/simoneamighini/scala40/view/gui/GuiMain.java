93ae492c545f4af08c2ab680aca190df1f49a558
Simone Amighini <simone.amighini@mail.polimi.it>
2025-04-03 16:17:26 +0200

    Add program's icons

--- src/main/java/it/simoneamighini/scala40/view/gui/GuiMain.java
+++ src/main/java/it/simoneamighini/scala40/view/gui/GuiMain.java
@@ -1,6 +1,7 @@
 package it.simoneamighini.scala40.view.gui;
 
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +14,13 @@ public class GuiMain extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         stage.setTitle("Scala 40");
+        stage.getIcons().addAll(
+                new Image(getClass().getResourceAsStream("images/icons/ICON_16X16.png")),
+                new Image(getClass().getResourceAsStream("images/icons/ICON_32X32.png")),
+                new Image(getClass().getResourceAsStream("images/icons/ICON_64X64.png")),
+                new Image(getClass().getResourceAsStream("images/icons/ICON_128X128.png")),
+                new Image(getClass().getResourceAsStream("images/icons/ICON_256X256.png"))
+        );
         stage.setFullScreen(false);
         stage.setMinWidth(1280);
         stage.setMinHeight(720);
